9a6bdeba79136d56be1964ca07b0ee2aba8bca28
syifashafza <121222546+syifashafza@users.noreply.github.com>
2023-06-19 15:17:43 +0700

    Add files via upload

---
+++ kombinatorial.cpp
@@ -0,0 +1,82 @@
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+//cr syifa
+class Madis {
+public:
+    void pilih();
+    void permutasi();
+    void kombinasi();
+private:
+    int q[100];
+};
+
+void Madis::pilih() {
+    int n;
+    cout << "\n1. Permutasi\n2. Kombinasi";
+    cout << "\n\nPilihan anda: ";
+    cin >> n;
+    if (n == 1)
+        permutasi();
+    else if (n == 2)
+        kombinasi();
+    else
+        cout << "\n\n**Selesai**\n\n";
+}
+
+void Madis::permutasi() {
+    system("cls");
+    int n, k;
+    cout << "\nMasukkan nilai n=";
+    cin >> n;
+    cout << "Masukkan nilai r=";
+    cin >> k;
+    if (k > n) {
+        cout << "\nNilai r harus kurang dari n";
+        permutasi();
+    }
+    int p = n - k;
+    int N = 1, K = 1;
+    for (int i = 1; i <= n; i++)
+        N *= i;
+    for (int i = 1; i <= p; i++)
+        K *= i;
+    cout << "\nMaka hasil permutasi: " << N / K;
+    cout << "\n\n";
+    pilih();
+}
+
+void Madis::kombinasi() {
+    system("cls");
+    int n, k;
+    cout << "\nMasukkan nilai n=";
+    cin >> n;
+    cout << "Masukkan nilai r=";
+    cin >> k;
+    if (k > n) {
+        cout << "\nNilai r harus kurang dari n";
+        kombinasi();
+    }
+    int p = n - k;
+    int N = 1, K = 1, P = 1;
+    for (int i = 1; i <= n; i++)
+        N *= i;
+    for (int i = 1; i <= k; i++)
+        K *= i;
+    for (int i = 1; i <= p; i++)
+        P *= i;
+    cout << "\n(n-r)! -> " << P;
+    cout << "\nMaka hasil kombinasi: " << N / (K * P);
+    cout << "\n\n";
+    pilih();
+}
+
+int main(int argc, char *argv[]) {
+    Madis z;
+    z.pilih();
+    system("PAUSE");
+    return EXIT_SUCCESS;
+}
+
